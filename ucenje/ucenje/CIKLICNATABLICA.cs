0df6e5abe5347da8348fc600974ceb85c47a2b32
LukaK24 <lkordic234@gmail.com>
2025-01-18 16:42:54 +0100

    Ciklicna tablica

---
+++ ucenje/ucenje/CIKLICNATABLICA.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ucenje
+{
+    internal class CIKLICNATABLICA
+    {
+        private static object martix;
+
+        public static void Izvedi() 
+        {
+             // unos dizmenije tablice
+            
+                int rows, cols;
+            Console.Write("Unesite broj redova (2-50): ");
+
+            while (int.TryParse(Console.ReadLine(), out rows) || rows < 2 || rows > 50) 
+            {
+                Console.WriteLine("Neispravan unos. Unesitze broj redova (2-50): ");
+            
+            }
+
+            Console.Write("Unesite broj kolona (2-50): ");
+
+            while (int.TryParse(Console.ReadLine(), out cols) || cols < 50) 
+            {
+                Console.Write("Neispravan unos. Unesite broj kolona (2-50): ");
+            }
+
+            // unos smjerova
+            Console.WriteLine("1. Dolje desno početak u smjeru kazaljke na satu");
+            Console.WriteLine("2. Dolje lijevo početak u smjeru kazaljke na satu");
+            Console.WriteLine("3. Gore lijevo početak u smjeru kazaljke na satu");
+            Console.WriteLine("4. Gore desno početak u smjeru kazaljke na satu");
+            Console.WriteLine("5. Dolje desno početak u kontra smjeru kazaljke na satu");
+            Console.WriteLine("6. Dolje lijevo početak u kontra smjeru kazaljke na satu");
+            Console.WriteLine("7. Gore lijevo početak u kontra smjeru kazaljke na satu");
+            Console.WriteLine("8. Gore desno početak u kontra smjeru kazaljke na satu");
+            Console.WriteLine("9. Sredina lijevo u smjeru kazaljke na satu");
+            Console.WriteLine("10. Sredina desno u smjeru kazaljke na satu");
+            Console.WriteLine("11. Sredina gore u smjeru kazaljke na satu");
+            Console.WriteLine("12. Sredina dolje u smjeru kazaljke na satu");
+            Console.WriteLine("13. Sredina lijevo u kontra smjeru kazaljke na satu");
+            Console.WriteLine("14. Sredina desno u kontra smjeru kazaljke na satu");
+            Console.WriteLine("15. Sredina gore u kontra smjeru kazaljke na satu");
+            Console.WriteLine("16. Sredina dolje u kontra smjeru kazaljke na satu");
+
+            int direction;
+            Console.Write("Unesite broj smejra: ");
+            while(int.TryParse(Console.ReadLine(),out direction) || direction < 1 || direction >16) 
+            {
+                Console.Write("Neispravan unos. Unesite broj smjera: ");
+            }
+
+            //generiranje matrice u odabranome smjeru
+
+            int[,] matrix = new int[rows, cols];
+            GeneriranjeMartix(martix, rows, cols, direction);
+
+            //ispis matrice
+            Printmatrix(matrix, rows, cols);
+
+            //pitanje za ponovni unos
+            Console.Write("Želite li napraviti još jednu matricu? (DA/NE): ");
+
+            while (Console.ReadLine().ToUpper() == "DA") ;
+
+
+
+
+        }
+
+        private static void Printmatrix(int[,] matrix, int rows, int cols)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void GeneriranjeMartix(object martix, int rows, int cols, int direction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void GeneriranjeMatrix(int[,] matrix, int rows, int cols, int direction) 
+        {
+            int num = 1;
+            int r = 0, C = 0;
+        }
+    }
+}
