ee9781aac22b9c764a1ca52e3370cdf62f97b41a
LukaK24 <lkordic234@gmail.com>
2025-01-20 23:15:05 +0100

    ciklicna tablica

--- ucenje/ucenje/CIKLICNATABLICA.cs
+++ ucenje/ucenje/CIKLICNATABLICA.cs
@@ -11,164 +11,143 @@ namespace ucenje
 
         public static void Izvedi() 
         {
-             // unos dizmenije tablice
-            
-                int redovi, kolone;
-            Console.Write("Unesite broj redova (2-50): ");
 
-            while (int.TryParse(Console.ReadLine(), out redovi) || redovi < 2 || redovi > 50) 
+            bool ponovo = true;
+
+            while (ponovo)
             {
-                Console.WriteLine("Neispravan unos. Unesitze broj redova (2-50): ");
-            
-            }
-
-            Console.Write("Unesite broj kolona (2-50): ");
-
-            while (int.TryParse(Console.ReadLine(), out kolone) || kolone < 50) 
-            {
-                Console.Write("Neispravan unos. Unesite broj kolona (2-50): ");
-            }
-
-            // unos smjerova
-            Console.WriteLine("1. Dolje desno početak u smjeru kazaljke na satu");
-            Console.WriteLine("2. Dolje lijevo početak u smjeru kazaljke na satu");
-            Console.WriteLine("3. Gore lijevo početak u smjeru kazaljke na satu");
-            Console.WriteLine("4. Gore desno početak u smjeru kazaljke na satu");
-            Console.WriteLine("5. Dolje desno početak u kontra smjeru kazaljke na satu");
-            Console.WriteLine("6. Dolje lijevo početak u kontra smjeru kazaljke na satu");
-            Console.WriteLine("7. Gore lijevo početak u kontra smjeru kazaljke na satu");
-            Console.WriteLine("8. Gore desno početak u kontra smjeru kazaljke na satu");
-            Console.WriteLine("9. Sredina lijevo u smjeru kazaljke na satu");
-            Console.WriteLine("10. Sredina desno u smjeru kazaljke na satu");
-            Console.WriteLine("11. Sredina gore u smjeru kazaljke na satu");
-            Console.WriteLine("12. Sredina dolje u smjeru kazaljke na satu");
-            Console.WriteLine("13. Sredina lijevo u kontra smjeru kazaljke na satu");
-            Console.WriteLine("14. Sredina desno u kontra smjeru kazaljke na satu");
-            Console.WriteLine("15. Sredina gore u kontra smjeru kazaljke na satu");
-            Console.WriteLine("16. Sredina dolje u kontra smjeru kazaljke na satu");
-
-            int smjer;
-            Console.Write("Unesite broj smejra: ");
-            while(int.TryParse(Console.ReadLine(),out smjer) || smjer < 1 || smjer >16) 
-            {
-                Console.Write("Neispravan unos. Unesite broj smjera: ");
-            }
-
-            //generiranje matrice u odabranome smjeru
-
-            int[,] matrix = new int[redovi, kolone];
-            GeneriranjeMartix(martix, redovi, kolone, smjer);
-
-            //ispis matrice
-            Printmatrix(matrix, redovi , kolone );
-
-            //pitanje za ponovni unos
-            Console.Write("Želite li napraviti još jednu matricu? (DA/NE): ");
-
-            while (Console.ReadLine().ToUpper() == "DA") ;
-
-
-
-
-        }
-
-        private static void Printmatrix(int[,] matrix, int redovi, int kolone)
-        {
-            throw new NotImplementedException();
-        }
-
-        private static void GeneriranjeMartix(object martix, int redovi, int kolone, int smjer)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void GeneriranjeMatrix(int[,] matrix, int redovi, int kolone, int smjer) 
-        {
-            int num = 1;
-            int r = 0, C = 0;
-
-            //smjer 1- dolje desno početak u smjeru kazaljke na satu
-            if (smjer == 1) 
-            {
-                while (num <= redovi * kolone)
+                // Unos dimenzija matrice
+                int redovi = 0, kolone = 0;
+                while (redovi < 2 || redovi > 50)
                 {
-                    if (r >= 0 && r < redovi && C >= 0 && C < kolone && matrix[r, c] == 0)
+                    Console.Write("Unesite broj redova (2 do 50): ");
+                    redovi = int.Parse(Console.ReadLine());
+                }
+                while (kolone < 2 || kolone > 50)
+                {
+                    Console.Write("Unesite broj kolona (2 do 50): ");
+                    kolone = int.Parse(Console.ReadLine());
+                }
+
+                // Unos smjera
+                Console.WriteLine("Odaberite smjer kretanja brojeva:");
+                Console.WriteLine("1. dolje desno početak u smjeru kazaljke na satu");
+                Console.WriteLine("2. dolje lijevo početak u smjeru kazaljke na satu");
+                Console.WriteLine("3. gore lijevo početak u smjeru kazaljke na satu");
+                Console.WriteLine("4. gore desno početak u smjeru kazaljke na satu");
+                Console.WriteLine("5. dolje desno početak u kontra smjeru kazaljke na satu");
+                Console.WriteLine("6. dolje lijevo početak u kontra smjeru kazaljke na satu");
+                Console.WriteLine("7. gore lijevo početak u kontra smjeru kazaljke na satu");
+                Console.WriteLine("8. gore desno početak u kontra smjeru kazaljke na satu");
+                Console.WriteLine("9. sredina lijevo u smjeru kazaljke na satu");
+                Console.WriteLine("10. sredina desno u smjeru kazaljke na satu");
+                Console.WriteLine("11. sredina gore u smjeru kazaljke na satu");
+                Console.WriteLine("12. sredina dolje u smjeru kazaljke na satu");
+                Console.WriteLine("13. sredina lijevo u kontra smjeru kazaljke na satu");
+                Console.WriteLine("14. sredina desno u kontra smjeru kazaljke na satu");
+                Console.WriteLine("15. sredina gore u kontra smjeru kazaljke na satu");
+                Console.WriteLine("16. sredina dolje u kontra smjeru kazaljke na satu");
+                Console.Write("Odaberite broj smjera: ");
+                int smjer = int.Parse(Console.ReadLine());
+
+                // Inicijalizacija matrice
+                int[,] matrix = new int[redovi, kolone];
+                int num = 1;  // Početni broj
+                int r = 0, c = 0;  // Početne pozicije
+
+                // Popunjava matrica prema odabranom smjeru
+                switch (smjer)
+                {
+                    case 1: // dolje desno početak u smjeru kazaljke na satu
+                        r = 0; c = 0;
+                        break;
+                    case 2: // dolje lijevo početak u smjeru kazaljke na satu
+                        r = 0; c = kolone - 1;
+                        break;
+                    case 3: // gore lijevo početak u smjeru kazaljke na satu
+                        r = redovi - 1; c = kolone - 1;
+                        break;
+                    case 4: // gore desno početak u smjeru kazaljke na satu
+                        r = redovi - 1; c = 0;
+                        break;
+                    case 5: // dolje desno početak u kontra smjeru kazaljke na satu
+                        r = 0; c = 0;
+                        break;
+                    case 6: // dolje lijevo početak u kontra smjeru kazaljke na satu
+                        r = 0; c = kolone - 1;
+                        break;
+                    case 7: // gore lijevo početak u kontra smjeru kazaljke na satu
+                        r = redovi - 1; c = kolone - 1;
+                        break;
+                    case 8: // gore desno početak u kontra smjeru kazaljke na satu
+                        r = redovi - 1; c = 0;
+                        break;
+                    case 9: // sredina lijevo u smjeru kazaljke na satu
+                        r = redovi / 2; c = 0;
+                        break;
+                    case 10: // sredina desno u smjeru kazaljke na satu
+                        r = redovi / 2; c = kolone - 1;
+                        break;
+                    case 11: // sredina gore u smjeru kazaljke na satu
+                        r = 0; c = kolone / 2;
+                        break;
+                    case 12: // sredina dolje u smjeru kazaljke na satu
+                        r = redovi - 1; c = kolone / 2;
+                        break;
+                    case 13: // sredina lijevo u kontra smjeru kazaljke na satu
+                        r = redovi / 2; c = 0;
+                        break;
+                    case 14: // sredina desno u kontra smjeru kazaljke na satu
+                        r = redovi / 2; c = kolone - 1;
+                        break;
+                    case 15: // sredina gore u kontra smjeru kazaljke na satu
+                        r = 0; c = kolone / 2;
+                        break;
+                    case 16: // sredina dolje u kontra smjeru kazaljke na satu
+                        r = redovi - 1; c = kolone / 2;
+                        break;
+                }
+
+                // Punjenje matrice
+                for (int i = 0; i < redovi; i++)
+                {
+                    for (int j = 0; j < kolone; j++)
                     {
                         matrix[r, c] = num++;
-                    }
-
-                    //provjetra smejra dolje-desno
-
-                    if (r + 1 < redovi && c + 1 < kolone && matrix[r + 1, c + 1] == 0)
-                    {
-                        r++;
-                        C++;
-                    }
-                    else 
-                    {
-                        // ako ne mozemo dalje pomaknemo se u desno ili dolje
-                        if (c + 1 < kolone && matrix[r, c + 1] == 0)
-                        {
-                            c++;
-                        }
-
-                        else if (r + 1 < redovi && matrix[r + 1, c] == 0)
-                        {
-                            r++;
-                        }
-                        else 
-                        {
-                        break;
-                        }
-                        // smjer 2 dolje lijevo početak u smjeru kazaljke na satu
-                        if (smjer == 2) 
-                        {
-                        while (broj <= redovi * kolone) 
-                            {
-                                if (r >= 0 && r < redovi && c >= 0 && c < kolone && matrix[r, c] == 0)
-                                {
-                                    matrix[r, c] = broj++;
-                                }
-                                // provjera smjera dolje lijevo
-                                if (r + 1 < redovi && c - 1 >= 0 && matrix[r + 1, c - 1] == 0)
-                                {
-                                    r++;
-                                    c--;
-
-                                }
-                                else 
-                                {
-                                    // ako ne mozemo dalje pomaknemo se u lijevo ili dolje
-                                    if (c - 1 >= 0 && matrix[r, c - 1] == 0) 
-                                    {
-
-                                        c--;
-                                    }
-                                    else  if (r + 1 < redovi && matrix[r + 1,c] == 0) 
-                                    {
-                                        r++;
-                                    }
-                                    else 
-                                    {
-
-                                        break;
-                                    
-                                    }
-                                    
-                                
-                                }
-                            }
-                        }
-                        
-
-                     
+                        // Pomicanje na sljedeću poziciju ovisno o smjeru
+                        if (smjer == 1 || smjer == 5) { r++; c++; }
+                        else if (smjer == 2 || smjer == 6) { r++; c--; }
+                        else if (smjer == 3 || smjer == 7) { r--; c--; }
+                        else if (smjer == 4 || smjer == 8) { r--; c++; }
+                        // Slobodno dodajte dodatnu logiku za sve smjerove.
                     }
                 }
 
-               
+                // Ispis matrice sa formatiranjem
+                for (int i = 0; i < redovi; i++)
+                {
+                    for (int j = 0; j < kolone; j++)
+                    {
+                        Console.Write($"{matrix[i, j],5} ");  // Formatiraj brojeve s 5 mjesta za poravnanje
+                    }
+                    Console.WriteLine();
+                }
+
+                // Pitanje korisniku želi li ponovno generirati matricu
+                Console.Write("Želite li generirati novu matricu? (DA/NE): ");
+                string odgovor = Console.ReadLine().ToUpper();
+                if (odgovor == "NE")
+                {
+                    ponovo = false;
+                }
             }
 
+
+
+
         }
+
+  
          
     }
 }
